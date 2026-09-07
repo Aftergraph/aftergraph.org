c185f51dd3ddf644a3a079a5d4046f12ea4f7452
Jonas Abde <147070826+JonasAbde@users.noreply.github.com>
2026-09-07 03:18:34 +0000

    feat: /status i launcher + sitemap (#17)

--- site/build-worker.cjs
+++ site/build-worker.cjs
@@ -66,6 +66,7 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://aftergraph.org/</loc><changefreq>weekly</changefreq><priority>1.0</priority></url>
   <url><loc>https://aftergraph.org/launch</loc><changefreq>weekly</changefreq><priority>0.8</priority></url>
+  <url><loc>https://aftergraph.org/status</loc><changefreq>daily</changefreq><priority>0.7</priority></url>
 </urlset>
 `;
 const headers = `const SECURE = {
