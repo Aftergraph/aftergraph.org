d30e2cc365bff0c6a1006c95b4f5480fdf407041
Jonas Abde <147070826+JonasAbde@users.noreply.github.com>
2026-09-26 13:36:07 +0200

    debug(m8-f15): inspect input transport methods

--- frontiers/m8-f15/pock_m8_f15_ci_driver.py
+++ frontiers/m8-f15/pock_m8_f15_ci_driver.py
@@ -205,7 +205,11 @@ def main() -> int:
         pos = source_text.find(needle)
         if pos >= 0:
             print("F15_TRANSPORT_SOURCE " + str(candidate), flush=True)
-            print(source_text[pos:pos + 14000], flush=True)
+            for method_name in ("def submit_input", "def next_input_as_worker", "def acknowledge_input_as_worker"):
+                method_pos = source_text.find(method_name)
+                if method_pos >= 0:
+                    print("F15_TRANSPORT_METHOD " + method_name, flush=True)
+                    print(source_text[method_pos:method_pos + 9000], flush=True)
             break
 
     tests = [
